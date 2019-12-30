7e949615312b2850b6f21db1ff63e541333d1318
Решетников Евгений Константинович <ekreshetnikov@edu.hse.ru>
2019-12-30 23:24:23 +0500

    Add project files.

---
+++ JSonParsing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSonParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
